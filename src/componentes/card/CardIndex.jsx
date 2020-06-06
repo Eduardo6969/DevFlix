47f6ce3dd06a790c4fb1014c3d757d438b9fc3a4
Eduardo <eduardo.santana33@etec.sp.gov.br>
2020-06-06 02:26:41 -0300

    Ajustar a barra de pesquisa e implementar sistema de naveggação

--- src/componentes/card/CardIndex.jsx
+++ src/componentes/card/CardIndex.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
 
 export default function CardSerie() {
-return(
+    return (
 
-<div className="card-serie">
-        <div className="poster-image">
-            <div className="card-detail">
-                <div className= "material-icons"> grade </div>  89% 
-                <div class="material-icons">watch_later</div>
-               
-   
-    <span>BadBoys Para sempre é um lançamento de 2020 que
-        conta a história de dois policiais parceiros que vivem o ...</span>
-               
+        <div className="card-serie">
+            <div class="material-icons watch-later">watch_later</div>
+            <div className="poster-image"> </div>
+        
+            <div className="card-detail"> </div>
+            <div className="texto-card">
+            <span>BadBoys Para sempre é um lançamento de 2020 que
+        conta a história de dois policiais parceiros que vivem o ... <a href="#"></a></span>
 
             </div>
+            <div className="material-icons star"> grade </div> <span className="porcent">89%</span>
 
+           
         </div>
 
 
-    </div>
+    )
 
-
- )
-    
 }
